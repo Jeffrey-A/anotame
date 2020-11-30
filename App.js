3f29c225ad5fe318330197d001606a83bfa281dc
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-11-29 21:34:56 -0500

    feat(package.json): installed react-native-contacts

--- App.js
+++ App.js
@@ -11,49 +11,71 @@ import {
 import ContactsWrapper from "react-native-contacts-wrapper";
 import ListItem from "./components/ListItem";
 
-export default function App(props) {
-  const [items, setItem] = useState([
-    {
-      name: "John Due 1",
-      date: "02/27/2020",
-      time: "10:00 pm",
-    },
-    {
-      name: "John Due 2",
-      date: "02/27/2020",
-      time: "10:00 pm",
-    },
-    {
-      name: "John Due 3",
-      date: "02/27/2020",
-      time: "10:00 pm",
-    },
-  ]);
-  return (
-    <View style={styles.mainWrapper}>
-      <View style={styles.header}>
-        <Text style={styles.logo}>Anotate</Text>
-        <Button title="Add" />
-      </View>
 
-      <FlatList
-        data={items}
-        renderItem={({ item }) => <ListItem item={item} />}
-      />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const items = [
+      {
+        name: "John Due 1",
+        date: "02/27/2020",
+        time: "10:00 pm",
+      },
+      {
+        name: "John Due 2",
+        date: "02/27/2020",
+        time: "10:00 pm",
+      },
+      {
+        name: "John Due 3",
+        date: "02/27/2020",
+        time: "10:00 pm",
+      },
+    ];
+
+    this.state = {
+      items: items,
+    }
+  }
+
+  add() {
+    
+  }
+
+  delete() {
+
+  }
+
+  render() {
+    const {items } = this.state;
+    return (
+      <View style={styles.mainWrapper}>
+        <View style={styles.header}>
+          <Text style={styles.logo}>Anotate</Text>
+          <Button title="Add" />
+        </View>
+  
+        <FlatList
+          data={items}
+          renderItem={({ item }) => <ListItem item={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   mainWrapper: {
     padding: 10,
-    paddingTop: 20,
   },
   header: {
     backgroundColor: "#fff",
     justifyContent: "space-between",
     flexDirection: "row",
     alignItems: "center",
+    paddingTop: 20,
+    paddingBottom: 20,
   },
   logo: {
     fontSize: 25,
